9e43f88f6c7e02d0598c4276f308d62b8095eac8
hadardv <hadardv14@gmail.com>
2024-12-12 17:59:18 +0200

    Initial commit

---
+++ app/src/main/java/logic/GameManager.kt
@@ -0,0 +1,25 @@
+package logic
+
+import androidx.appcompat.widget.AppCompatImageView
+
+class GameManager (private val hearts: Array<AppCompatImageView>) {
+
+    fun startGame(){
+        TODO()
+    }
+
+    fun moveHarry(index:Int) {
+        TODO()
+    }
+    fun updateLives(){
+        TODO()
+    }
+
+    fun checkCollision(){
+        TODO()
+    }
+
+    fun isGameOver(){
+        TODO()
+    }
+}
